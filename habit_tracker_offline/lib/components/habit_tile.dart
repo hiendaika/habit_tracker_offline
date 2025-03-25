431faee35e2784f6e030fde7bbb21564eea6e9f5
hiendaika <mrphonglinh2605@gmail.com>
2025-03-25 23:02:58 +0700

    Update delete habit

--- habit_tracker_offline/lib/components/habit_tile.dart
+++ habit_tracker_offline/lib/components/habit_tile.dart
@@ -1,34 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HabitTile extends StatelessWidget {
   final String text;
   final bool isCompleted;
-  void Function(bool?)? onChanged;
-  HabitTile({
+  final void Function(bool?)? onChanged;
+  final void Function(BuildContext)? onPressedEdit;
+  final void Function(BuildContext)? onPressedDelete;
+  const HabitTile({
     super.key,
     required this.isCompleted,
     required this.text,
     required this.onChanged,
+    required this.onPressedEdit,
+    required this.onPressedDelete,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color:
-            isCompleted
-                ? Colors.green
-                : Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(8),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        children: [
+          //Edit option
+          SlidableAction(
+            onPressed: onPressedEdit,
+            icon: Icons.edit,
+            backgroundColor: Colors.grey.shade800,
+            borderRadius: BorderRadius.circular(8),
+          ),
+
+          // Delete option
+          SlidableAction(
+            onPressed: onPressedDelete,
+            icon: Icons.delete,
+            backgroundColor: Colors.red.shade400,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ],
       ),
-      padding: EdgeInsets.all(12),
-      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-      child: ListTile(
-        title: Text(text),
-        leading: Checkbox(
-          value: isCompleted,
-          onChanged: onChanged,
-          activeColor: Colors.green,
+      child: GestureDetector(
+        onTap: () {
+          if (onChanged != null) {
+            // toggle completion status
+            onChanged!(!isCompleted);
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color:
+                isCompleted
+                    ? Colors.green
+                    : Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+          child: ListTile(
+            title: Text(
+              text,
+              style: TextStyle(
+                color:
+                    isCompleted
+                        ? Colors.white
+                        : Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            leading: Checkbox(
+              value: isCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.green,
+            ),
+          ),
         ),
       ),
     );
--- habit_tracker_offline/lib/pages/home_page.dart
+++ habit_tracker_offline/lib/pages/home_page.dart
@@ -15,14 +15,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController myController = TextEditingController();
+
   @override
   void initState() {
-    // TODO: implement initState
+    //read existing habits on app start up
+    Provider.of<HabitDatabase>(context, listen: false).fetchHabits();
+
     super.initState();
-    readAllHabit();
   }
 
-  void createNewHabit(BuildContext context) {
+  void createNewHabit() {
     showDialog(
       context: context,
       builder:
@@ -40,11 +42,14 @@ class _HomePageState extends State<HomePage> {
                   Navigator.pop(context);
                   myController.clear();
                 },
+                child: Text('Create'),
               ),
               MaterialButton(
                 onPressed: () {
                   Navigator.pop(context);
+                  myController.clear();
                 },
+                child: Text('Cancel'),
               ),
             ],
           ),
@@ -64,25 +69,97 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  //edit habit box
+  void editHabitBox(Habit habit) {
+    //Set text controller
+    myController.text = habit.name;
+
+    //show dialog
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text('Edit habit'),
+            content: TextField(
+              controller: myController,
+              decoration: InputDecoration(hintText: 'Edit a habit'),
+            ),
+            actions: [
+              MaterialButton(
+                onPressed: () {
+                  String newHabitName = myController.text;
+                  context.read<HabitDatabase>().updateHabitName(
+                    habit.id,
+                    newHabitName,
+                  );
+                  Navigator.pop(context);
+                  myController.clear();
+                },
+                child: Text('Save'),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  myController.clear();
+                },
+                child: Text('Cancel'),
+              ),
+            ],
+          ),
+    );
+  }
+
+  //delete habit box
+  void deleteHabitBox(Habit habit) {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text('Delete habit'),
+            content: Text('Are you sure you want to delete this habit?'),
+            actions: [
+              MaterialButton(
+                onPressed: () {
+                  context.read<HabitDatabase>().deleteHabit(habit.id);
+                  Navigator.pop(context);
+                },
+                child: Text('Delete'),
+              ),
+              MaterialButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('Cancel'),
+              ),
+            ],
+          ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(elevation: 0, backgroundColor: Colors.transparent),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       drawer: MyDrawer(),
       body: _buildHabitList(),
       floatingActionButton: FloatingActionButton(
         elevation: 0,
-        onPressed: () => createNewHabit,
+        onPressed: () => createNewHabit(),
         backgroundColor: Theme.of(context).colorScheme.tertiary,
-        child: const Icon(Icons.add),
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
       ),
     );
   }
 
   Widget _buildHabitList() {
     //habit db
-    final habitDatabase = context.read<HabitDatabase>();
+    final habitDatabase = context.watch<HabitDatabase>();
 
     //Current habit
     List<Habit> habits = habitDatabase.currentHabits;
@@ -97,11 +174,15 @@ class _HomePageState extends State<HomePage> {
         //check if habit is complete today
         bool isCompleteToday = isHabitCompletedToday(item.completedDays);
 
-        // return habit title
+        // return habit title UI
         return HabitTile(
           text: item.name,
           isCompleted: isCompleteToday,
-          onChanged: (value) {},
+          onChanged: (value) {
+            checkHabitOnOff(value, item);
+          },
+          onPressedEdit: (context) => editHabitBox(item),
+          onPressedDelete: (context) => deleteHabitBox(item),
         );
       },
     );
